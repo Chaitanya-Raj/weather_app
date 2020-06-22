aa44ceef4b2b6cb680e9bc3d92f9fa60da347c26
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2020-06-22 16:40:12 +0530

    fix height bug for firefox

--- src/components/search.js
+++ src/components/search.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Search = ({ search, onChange, onSubmit }) => {
   return (
-    <div className="search container h-100">
-      <div className="d-flex justify-content-center h-100">
+    <div className="search container">
+      <div className="d-flex justify-content-center">
         <form className="searchbar" onSubmit={onSubmit}>
           <input
             className="search_input"
