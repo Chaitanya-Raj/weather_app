2f373f8c111f8fe84d9db9ea227d5ccffbfbee7d
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2020-11-03 17:16:13 +0530

    change name

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,7 +4,7 @@ import logo from "../images/github.png";
 const Nav = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark justify-content-between">
-      <h1 className="navbar-brand">Weather App</h1>
+      <h1 className="navbar-brand">Weather</h1>
       <a href="https://chaitanya-raj.github.io" className=" mr-sm-2">
         <img src={logo} alt="github" />
       </a>
